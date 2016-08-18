18e3c6ae20c3009cff7ba67965e50b3f9a63632e
n8e <godmetweenciati@gmail.com>
2016-08-18 15:54:44 +0300

    Server configuration file

---
+++ server.js
@@ -0,0 +1,53 @@
+(() => {
+  'use-strict';
+  if (!process.env.NODE_ENV) {
+    require('dotenv').load();
+  }
+
+  /* eslint no-console: 0 */
+  const path = require('path'),
+    express = require('express'),
+    webpack = require('webpack'),
+    config = require('./server/config'),
+    webpackMiddleware = require('webpack-dev-middleware'),
+    webpackHotMiddleware = require('webpack-hot-middleware'),
+    webpackConfig = require('./webpack.config.js'),
+    isDeveloping = process.env.NODE_ENV !== 'production',
+    port = config.port,
+    app = express();
+
+  if (isDeveloping) {
+    const compiler = webpack(webpackConfig);
+    const middleware = webpackMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+      contentBase: 'src',
+      stats: {
+        colors: true,
+        hash: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false,
+        modules: false
+      }
+    });
+
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    app.get('*', (req, res) => {
+      res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+      res.end();
+    });
+  } else {
+    app.use(express.static(__dirname + '/dist'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
+  }
+
+  app.listen(port, '0.0.0.0', (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+  });
+})();
