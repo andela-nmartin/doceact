e52386855862a31560297353c3eda3dacc89fef4
n8e <godmetweenciati@gmail.com>
2016-08-23 10:38:24 +0300

    Add roles controller for routes

--- server/controllers/roles.js
+++ server/controllers/roles.js
@@ -1,32 +1,36 @@
-(function() {
+(() => {
   'use strict';
   // get the required models and db connection
-  var Role = require('../models/roles');
+  const Role = require('../models/roles');
 
   module.exports = {
     // gets all the saved roles from the db
-    get: function(req, res) {
-      Role.find({}, function(err, roles) {
-        if (err) {
-          res.send(err);
-          return;
-        }
-        res.json(roles);
-      });
+    get: (req, res) => {
+      Role
+        .find({})
+        .exec((err, roles) => {
+          if (err) {
+            res.send(err);
+            return;
+          } else {
+            res.json(roles);
+          }
+        });
     },
 
     // creates a role in the db
-    create: function(req, res) {
+    create: (req, res) => {
       var role = new Role({
         id: req.body.id,
         title: req.body.title
       });
-      role.save(function(err) {
+      role.save((err) => {
         if (err) {
           res.status(409).send(err);
           return;
         }
         res.json({
+          role: role,
           success: true,
           message: 'Role has been created!'
         });
