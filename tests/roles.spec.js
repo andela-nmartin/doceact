e6ecf7eb97cecc453dde5d711be277da1d99748b
n8e <godmetweenciati@gmail.com>
2016-08-23 10:35:27 +0300

    Add roles tests

---
+++ tests/roles.spec.js
@@ -0,0 +1,41 @@
+const url = 'http://localhost:4000';
+const expect = require('chai').expect;
+const request = require('superagent');
+
+describe('Roles', () => {
+  it('validates that the seeded roles are stored in database', (done) => {
+    request
+      .get(url + '/api/users/roles')
+      .end((err, res) => {
+        // expected responses after seeding
+        expect(res.status).to.equal(200);
+        expect(res.body.length).to.equal(2);
+        if (res.body[0].id === 1) {
+          expect(res.body[0].title).to.equal('Administrator');
+        } else if (res.body[0].id === 2) {
+          expect(res.body[0].title).to.equal('User');
+        } else if (res.body[1].id === 1) {
+          expect(res.body[1].title).to.equal('Administrator');
+        } else if (res.body[1].id === 2) {
+          expect(res.body[1].title).to.equal('User');
+        }
+        done();
+      });
+  });
+
+  it('validates that a new role created has a unique title', (done) => {
+    request
+      .post(url + '/api/users/roles')
+      .send({
+        id: 1,
+        title: 'Administrator'
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(409);
+        expect(res.body.code).to.equal(11000);
+        expect(res.body.index).to.equal(0);
+        expect(res.body.errmsg).to.contain('E11000 duplicate key error');
+        done();
+      });
+  });
+});
